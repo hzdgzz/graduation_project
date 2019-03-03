8bce79863195bc4e8a7b196c85bd4c15098162e7
dgzz <1164549585@qq.com>
2019-03-03 19:54:28 +0800

    前端页面，静态页面轮廓大体实现，后期再进行美化和完善

--- static/js/index.js
+++ static/js/index.js
@@ -1,33 +1,16 @@
 $(function(){
     // 获取元素登录或注册
-    var btn = $('.indexbtn')
     var sureperson = $('#sureperson')
-    var formbox1 = $('#formbox1')
-    var formbox2 = $('#formbox2')
     var personbtn = $('#personbtn')
     var chooseperson = $('#chooseperson')
-
-    // 表单默认隐藏
-    formbox1.hide()
-    formbox2.hide()
-    // 事件委托,监控点击登录和注册
-    btn.delegate('input','click',function(){
-        if($(this).prop('type') == 'submit'){
-            alert('f')
-        }else if($(this).prop('type') == 'reset'){
-            alert('b')
-        }
-    })
+    
     // 监控身份登录确定按钮
     sureperson.click(function(){
         // 点击进入相应的人员界面，随后输入账号和密码
         if(chooseperson.prop('value') == '0'){
-            personbtn.hide()
-            formbox2.show()
-
+            window.location.href = 'SuperadminLogin.html';
         }else{
-            personbtn.hide()
-            formbox1.show()
+            window.location.href = 'AdminLogin.html';
         }
         
     })
