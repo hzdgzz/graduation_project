f15ed0e8ce8523508bee5ab54d004c68855141c7
dgzz <1164549585@qq.com>
2019-03-14 11:19:45 +0800

    从后台数据库拿管理员数据，传递给前端

--- person/modules/Admin/views.py
+++ person/modules/Admin/views.py
@@ -1,6 +1,10 @@
+from flask import current_app, jsonify
 from flask import render_template
+
+from person.utils.response_code import RET
 from . import Admin
 # 超级管理员登录界面
 @Admin.route("/Admin.html")
 def Admin():
+
     return render_template('Admin.html')
--- person/modules/Superson/views.py
+++ person/modules/Superson/views.py
@@ -1,15 +1,29 @@
+from flask import current_app, jsonify
 from flask import render_template
+
+from person.utils.response_code import RET
 from . import Superson
+from person.models import Admin
 
 
 @Superson.route("/")
 def index():
-
     return render_template('index.html')
 
 
 @Superson.route("/Supersons.html")
 def Supersons():
-    return render_template('Supersons.html')
-
-
+    # 从数据库获取数据
+    try:
+        admins = Admin.query.all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='查询管理员数据失败')
+    # 判断查询结果
+    if not admins:
+        return jsonify(errno=RET.NODATA, errmsg='无管理员数据')
+    # 定义列表，存储数据
+    data = {
+        'admins':admins
+    }
+    return render_template('Supersons.html', data=data)
